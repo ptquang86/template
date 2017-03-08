034d2e47bfc8c6eb4bc44d2e985e5e071f23cbb3
Quang Pham <ptquang86@gmail.com>
2017-03-08 20:48:21 +0700

    listview

---
+++ app/controllers/ui/listview.js
@@ -0,0 +1,45 @@
+var args = $.args;
+
+init();
+function init() {
+
+}
+
+exports.load = function(data) {
+	$.collection.reset(data);
+    toggleAI(false);
+};
+
+exports.unload = function() {
+	$.destroy();
+};
+
+function toggleAI(visible) {
+  	if (visible) {
+		$.ai.height = Titanium.UI.SIZE;
+		$.ai.top = 20;
+		$.ai.show();
+	} else {
+		$.ai.hide();
+		$.ai.height = 0;
+		$.ai.top = 0;
+	}
+}
+exports.toggleAI = toggleAI;
+
+function doFilter(collection) {
+	if (0) {
+		// return collection.where({ is_selected: 1 });
+		return collection.filter(function(model, index, o) {
+	      	return model.get('is_selected') == 1 || model.get('is_purchased') == 1;
+	    });
+	} else {
+	    return collection.models;
+	}
+}
+
+function doTransform(model) {
+	var o = model.toJSON();
+	// o.template = 'template';
+	return o;
+}
--- app/controllers/window.js
+++ app/controllers/window.js
@@ -1,8 +1,12 @@
 var args = $.args;
 
-exports.load = function() {};
+exports.load = function() {
+    getData();
+};
 
-exports.unload = function() {};
+exports.unload = function() {
+    unloadData();
+};
 
 function goBack() {
     Alloy.Globals.WinManager.loadPrevious();
@@ -10,4 +14,28 @@ function goBack() {
 
 function showProfile() {
     alert('TODO: showProfile');
+}
+
+// ==
+
+function getData() {
+    $.listview.toggleAI();
+    setTimeout(loadData, 3000);
+}
+
+function loadData() {
+    var data = [];
+    for (var i = 0, ii = 10; i < ii; i++) {
+        data.push({
+            id: i,
+            icon: 'http://lorempixel.com/72/72/',
+            title: 'title',
+            subtitle: 'subtitle'
+        });
+    }
+    $.listview.load(data);
+}
+
+function unloadData() {
+    $.listview.unload();
 }
