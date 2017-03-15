8f2ee8dd753b980d9c0ff6980cbb2ea35c161354
Quang Pham <ptquang86@gmail.com>
2017-03-15 19:56:46 +0700

    textfield

--- app/controllers/ui/textfield.js
+++ app/controllers/ui/textfield.js
@@ -1,2 +1,55 @@
-// Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
+
+init();
+function init() {
+    var exclude = ['id', 'children', 'hintText'];
+    var textfields = [
+        'autocapitalization', 'autocorrect', 'clearButtonMode',
+        'hintTextColor', 'keyboardType', 'passwordMask',
+        'returnKeyType', 'softKeyboardOnFocus'
+    ];
+
+    for (var key in args) {
+        if (args.hasOwnProperty(key)) {
+            if (textfields.indexOf(key) != -1) {
+                $.txt[key] = args[key];
+            } else if (exclude.indexOf(key) == -1) {
+                $.container[key] = args[key];
+            }
+        }
+    }
+
+    var hintText = args.hintText;
+    if (hintText) {
+        if (OS_IOS) {
+            $.txt.attributedHintText = Ti.UI.createAttributedString({
+                text: hintText,
+                attributes: [
+                    {
+                        type: Ti.UI.ATTRIBUTE_FOREGROUND_COLOR,
+                        value: args.hintTextColor || '#555',
+                        range: [0, hintText.length]
+                    }
+                ]
+            });
+        } else {
+            $.txt.hintText = hintText;
+        }
+    }
+}
+
+exports.setValue = function(value) {
+    $.txt.value = value;
+};
+
+exports.getValue = function(value) {
+    return $.txt.value;
+};
+
+exports.focus = function() {
+    $.txt.focus();
+};
+
+function txtReturn(e) {
+    return $.trigger('return', { id: args.id, value: this.value });
+}
